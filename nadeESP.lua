752245734dc7174e27b17b04c409dd998679c41f
Nexxed <headstruck@protonmail.ch>
2021-01-11 20:03:11 +0000

    Update nadeESP.lua
    Changes:
    - Updated with the latest code from the repo
    - Added fade-out over time checkbox for smoke and molotovs
    - Cleaned-up code for better readability
    - Removed API table since it actually further slows things down
    
     I also (yet again) moved the "selected" checks to the paint function again - it's better to have it there than cluttered in each draw function.

--- nadeESP.lua
+++ nadeESP.lua
@@ -1,34 +1,16 @@
 ---------------------
----------API---------
----------------------
-local api = {
-    client = {
-        set_event_callback = client.set_event_callback
-    },
-    entity = {
-        get_all = entity.get_all,
-        get_prop = entity.get_prop
-    },
-    globals = {
-        tickcount = globals.tickcount,
-    },
-    renderer = {
-        world_to_screen = renderer.world_to_screen,
-        circle_outline = renderer.circle_outline,
-        circle = renderer.circle
-    },
-    ui = {
-        new_color_picker = ui.new_color_picker,
-        new_multiselect = ui.new_multiselect,
-        new_slider = ui.new_slider,
-        set_visible = ui.set_visible
-        get = ui.get,
-    }
-}
----------------------
 ---------LIB---------
 ---------------------
 local images = require "gamesense/images"
+
+function Color(R, G, B, A)
+    return {
+        r = R or 0,
+        g = G or 0,
+        b = B or 0,
+        a = A or 255
+    }
+end
 ---------------------
 ------VARIABLES------
 ---------------------
@@ -47,9 +29,34 @@ local var = {
 ----------------------
 local ui_additions = {
     nade_esp = {
-        ui_enabled = api.ui.new_multiselect("VISUALS", "Other ESP", "Nade ESP", "Smoke", "Molotov"),
-        outline_color = api.ui.new_color_picker("VISUALS", "Other ESP", "Nade ESP outline color", 149, 184, 6, 255),
-        icon_size = api.ui.new_slider("VISUALS", "Other ESP", "Nade ESP icon size", 1, 35, 25, true, "px")
+        ui_enabled = ui.new_multiselect("VISUALS", "Other ESP", "Nade ESP", "Smoke", "Molotov"),
+        ui_settings = ui.new_combobox("VISUALS", "Other ESP", "Nade ESP settings", "Smoke", "Molotov"),
+
+        settings = {
+            molotov = {
+                fade_out = ui.new_checkbox("VISUALS", "Other ESP", "Fade-out over time"),
+                background_color_label = ui.new_label("VISUALS", "Other ESP", "Background"),
+                background_color = ui.new_color_picker("VISUALS", "Other ESP", "background_color", 0, 0, 0, 200),
+                icon_color_label = ui.new_label("VISUALS", "Other ESP", "Icon"),
+                icon_color = ui.new_color_picker("VISUALS", "Other ESP", "icon_color", 255, 255, 255, 255),
+                indicator_color_label = ui.new_label("VISUALS", "Other ESP", "Indicator"),
+                indicator_color = ui.new_color_picker("VISUALS", "Other ESP", "indicator_color", 255, 255, 255, 255),
+                hvhmode = ui.new_checkbox("VISUALS", "Other ESP", "HvH-mode"),
+                hvh_safe_color_label = ui.new_label("VISUALS", "Other ESP", "  safe"),
+                hvh_safe_color = ui.new_color_picker("VISUALS", "Other ESP", "hvh_icon_safe_color", 0, 255, 0, 255),
+                hvh_unsafe_color_label = ui.new_label("VISUALS", "Other ESP", "  unsafe"),
+                hvh_unsafe_color = ui.new_color_picker("VISUALS", "Other ESP", "hvh_icon_unsafe_color", 255, 0, 0, 255)
+            },
+            smoke = {
+                fade_out = ui.new_checkbox("VISUALS", "Other ESP", "Fade-out over time"),
+                background_color_label = ui.new_label("VISUALS", "Other ESP", "Background"),
+                background_color = ui.new_color_picker("VISUALS", "Other ESP", "background_color", 0, 0, 0, 200),
+                icon_color_label = ui.new_label("VISUALS", "Other ESP", "Icon"),
+                icon_color = ui.new_color_picker("VISUALS", "Other ESP", "icon_color", 255, 255, 255, 255),
+                indicator_color_label = ui.new_label("VISUALS", "Other ESP", "Indicator"),
+                indicator_color = ui.new_color_picker("VISUALS", "Other ESP", "indicator_color", 255, 255, 255, 255)
+            }
+        }
     }
 }
 ---------------------
@@ -57,70 +64,131 @@ local ui_additions = {
 ---------------------
 -- nade esp
 local function draw_inferno()
-    local molos = api.entity.get_all("CInferno")
+    local hvh_mode_enabled = ui.get(ui_additions.nade_esp.settings.molotov.hvhmode)
+    local background_color = Color(ui.get(ui_additions.nade_esp.settings.molotov.background_color))
 
+    local icon_color = nil
+    local icon_color2 = nil
+    local indicator_color = nil
+    local indicator_color2 = nil
+
+    if hvh_mode_enabled == true then
+        icon_color = Color(ui.get(ui_additions.nade_esp.settings.molotov.hvh_unsafe_color))
+        indicator_color = icon_color
+
+        icon_color2 = Color(ui.get(ui_additions.nade_esp.settings.molotov.hvh_safe_color))
+        indicator_color2 = icon_color2
+    else
+        icon_color = Color(ui.get(ui_additions.nade_esp.settings.molotov.icon_color))
+        indicator_color = Color(ui.get(ui_additions.nade_esp.settings.molotov.indicator_color))
+    end
+
+    local molos = entity.get_all("CInferno")
     for i = 1, #molos do
         local index = molos[i]
 
-        local x, y, z = api.entity.get_prop(index, "m_vecOrigin")
-        local endTime = api.entity.get_prop(index, "m_nFireEffectTickBegin") + var.nade_data.inferno.time
+        local x, y, z = entity.get_prop(index, "m_vecOrigin")
+        local endTime = entity.get_prop(index, "m_nFireEffectTickBegin") + var.nade_data.inferno.time
+        local local_player = entity.get_local_player()
+        local owner = entity.get_prop(index, "m_hOwnerEntity")
+        local enemy = entity.is_enemy(owner)
 
-        local curtime = api.globals.tickcount()
+        local curtime = globals.tickcount()
         local dif = endTime - curtime
         local percentage = (dif * 100 / var.nade_data.inferno.time) / 100
 
-        local posX, posY = api.renderer.world_to_screen(x, y, z)
+        local posX, posY = renderer.world_to_screen(x, y, z)
 
         if posX ~= nil and posY ~= nil then
-            local color = {api.ui.get(ui_additions.nade_esp.outline_color)}
+            local _background_color = background_color
+            local _indicator_color = {}
+            local _icon_color = {}
 
-            api.renderer.circle(posX, posY, 0, 0, 0, 200, 20, 0, 1)
-            api.renderer.circle_outline(posX, posY, color[1], color[2], color[3], color[4], 18, 270, percentage, 2)
-            local width, height = images.get_panorama_image("icons/equipment/molotov.svg"):measure(nil, api.ui.get(ui_additions.nade_esp.icon_size))
-            images.get_panorama_image("icons/equipment/molotov.svg"):draw(posX - (width / 2) + 1, posY - (height / 2) - 2, width, height)
+            if enemy == false and hvh_mode_enabled == true and owner ~= local_player then
+                _indicator_color = indicator_color2
+                _icon_color = icon_color2
+            else
+                _indicator_color = indicator_color
+                _icon_color = icon_color
+            end
+
+            if(ui.get(ui_additions.nade_esp.settings.molotov.fade_out)) then
+                local alpha = (255 * ((percentage) * 100) / 100)
+
+                _background_color.a = alpha
+                _indicator_color.a = alpha
+                _icon_color.a = alpha
+            end
+
+            renderer.circle(posX, posY, background_color.r, background_color.g, background_color.b, background_color.a, 20, 0, 1)
+
+            local width, height = images.get_panorama_image("hud/deathnotice/icon-molotov.png"):measure(nil, 20)
+            renderer.circle_outline(posX, posY, _indicator_color.r, _indicator_color.g, _indicator_color.b, _indicator_color.a, 18, 270, percentage, 2)
+            images.get_panorama_image("hud/deathnotice/icon-molotov.png"):draw(posX - (width / 2), posY - (height / 2), width, height, _icon_color.r, _icon_color.g, _icon_color.b, _icon_color.a)
         end
     end
 end
 
 local function draw_smoke()
-    local smokes = api.entity.get_all("CSmokeGrenadeProjectile")
+    local background_color = Color(ui.get(ui_additions.nade_esp.settings.smoke.background_color))
+    local icon_color = Color(ui.get(ui_additions.nade_esp.settings.smoke.icon_color))
+    local indicator_color = Color(ui.get(ui_additions.nade_esp.settings.smoke.indicator_color))
 
+    local smokes = entity.get_all("CSmokeGrenadeProjectile")
     for i = 1, #smokes do
         local index = smokes[i]
-        local smokeEffect = api.entity.get_prop(index, "m_bDidSmokeEffect")
+        local smokeEffect = entity.get_prop(index, "m_bDidSmokeEffect")
+
         -- wait for m_nSmokeEffectTickBegin
-        if smokeEffect ~= 1 then
-            goto skip_to_next
+        if smokeEffect == 1 then
+            local x, y, z = entity.get_prop(index, "m_vecOrigin")
+            local endTime = entity.get_prop(index, "m_nSmokeEffectTickBegin") + var.nade_data.smoke.time
+
+            local curtime = globals.tickcount()
+            local dif = endTime - curtime
+            local percentage = (dif * 100 / var.nade_data.smoke.time) / 100
+
+            local posX, posY = renderer.world_to_screen(x, y, z)
+
+            if posX ~= nil and posY ~= nil then
+                local _background_color = background_color
+                local _indicator_color = indicator_color
+                local _icon_color = icon_color
+
+                if(ui.get(ui_additions.nade_esp.settings.smoke.fade_out)) then
+                    local alpha = (255 * ((percentage) * 100) / 100)
+
+                    _background_color.a = alpha
+                    _indicator_color.a = alpha
+                    _icon_color.a = alpha
+                end
+
+                renderer.circle(posX, posY, _background_color.r, _background_color.g, _background_color.b, _background_color.a, 20, 0, 1)
+                renderer.circle_outline(posX, posY, _indicator_color.r, _indicator_color.g, _indicator_color.b, _indicator_color.a, 18, 270, percentage, 2)
+
+                local width, height = images.get_panorama_image("hud/deathnotice/icon-smokegrenade.png"):measure(nil, 20)
+                images.get_panorama_image("hud/deathnotice/icon-smokegrenade.png"):draw(posX - (width / 2), posY - (height / 2), width, height, _icon_color.r, _icon_color.g, _icon_color.b, _icon_color.a)
+            end
         end
-
-        local x, y, z = api.entity.get_prop(index, "m_vecOrigin")
-        local endTime = api.entity.get_prop(index, "m_nSmokeEffectTickBegin") + var.nade_data.smoke.time
-
-        local curtime = api.globals.tickcount()
-        local dif = endTime - curtime
-        local percentage = (dif * 100 / var.nade_data.smoke.time) / 100
-
-        local posX, posY = api.renderer.world_to_screen(x, y, z)
-
-        if posX ~= nil and posY ~= nil then
-            local color = {api.ui.get(ui_additions.nade_esp.outline_color)}
-
-            api.renderer.circle(posX, posY, 0, 0, 0, 200, 20, 0, 1)
-            api.renderer.circle_outline(posX, posY, color[1], color[2], color[3], color[4], 18, 270, percentage, 2)
-            local width, height = images.get_panorama_image("icons/equipment/smokegrenade.svg"):measure(nil, api.ui.get(ui_additions.nade_esp.icon_size))
-            images.get_panorama_image("icons/equipment/smokegrenade.svg"):draw(posX - (width / 2) + 1, posY - (height / 2) - 1, width, height)
-        end
-
-        ::skip_to_next::
     end
+end
+
+local function nade_esp_setvisible()
+    local active_settings_tab = ui.get(ui_additions.nade_esp.ui_settings)
+
+    table.foreach(ui_additions.nade_esp.settings, function(index)
+        local status = index:lower() == active_settings_tab:lower()
+
+        table.foreach(ui_additions.nade_esp.settings[index], function(index1)
+            ui.set_visible(ui_additions.nade_esp.settings[index][index1], status)
+        end)
+    end)
 end
 --------------------
 -------EVENTS-------
 --------------------
 local function paint()
-    local selected = api.ui.get(ui_additions.nade_esp.ui_enabled)
-
-    api.ui.set_visible(ui_additions.nade_esp.icon_size, #selected > 0)
+    local selected = ui.get(ui_additions.nade_esp.ui_enabled)
 
     if selected[1] == "Molotov" or selected[2] == "Molotov" then
         draw_inferno()
@@ -133,7 +201,6 @@ end
 --------------------
 -----INITIALIZE-----
 --------------------
-local function init()
-    api.client.set_event_callback("paint", paint)
-end
-init()
+nade_esp_setvisible()
+ui.set_callback(ui_additions.nade_esp.ui_settings, nade_esp_setvisible)
+client.set_event_callback("paint", paint)
